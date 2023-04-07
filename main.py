d04f1b46858071b37905a42e47368633371ea2ac
= <44868912+jonathanjohn47@users.noreply.github.com>
2023-04-07 07:25:00 +0530

    creating separate file for classes.

--- main.py
+++ main.py
@@ -1,91 +1,21 @@
 import pandas
 import numpy as np
 import math
-
-"""You need to write a program using Python to choose the best four ideal functions from a set of fifty functions based on how well they fit the training data. 
-Then you must use the test data to determine whether each point can be assigned to the four chosen functions, and if so, 
-map it to the function with the least deviation. The program must then save the mapping and deviation information and display it visually. 
-Finally, you must ensure that any deviations between the mapped data and the ideal functions do not exceed the largest deviation between the training 
-set and the ideal function by a factor of sqrt(2)."""
-
-
-class Database:
-    def find_sum_of_least_squares(self, x, y):
-        x = np.array(x)
-        y = np.array(y)
-        difference = np.subtract(x, y)
-        square = np.square(difference)
-        sumOfSquares = np.sum(square)
-
-        return sumOfSquares
-
-    def find_least_squares(self, x, y):
-        x = np.array(x)
-        y = np.array(y)
-        difference = np.subtract(x, y)
-        square = np.square(difference)
-        return pandas.DataFrame(square)
-
-    def find_deviation(self, x, y):
-        x = np.array(x)
-        y = np.array(y)
-        difference = np.subtract(x, y)
-        return pandas.DataFrame(difference)
-
-
-class Train(Database):
-    def load_training_data(self):
-        self.train = pandas.read_csv("dataset/train.csv")
-        return self.train
-
-    def get_deviation(self, x):
-        self.deviation = self.find_deviation(x, self.train.iloc[:, 1])
-        return self.deviation
-
-
-class Test(Database):
-    def load_test_data(self):
-        self.test = pandas.read_csv("dataset/test.csv")
-        return self.test
-
-
-class Ideal(Database):
-    def load_ideal_data(self):
-        self.ideal = pandas.read_csv("dataset/ideal.csv")
-        return self.ideal
-
-    def find_ideal(self, x):
-        least_squares = []
-        database = Database()
-        least_squares = [database.find_sum_of_least_squares(x, self.ideal.iloc[:, i]) for i in
-                         range(len(self.ideal.columns))]
-
-        first_four_least_squares = sorted(least_squares)[:4]
-        indices = []
-        for i in first_four_least_squares:
-            indices.append(least_squares.index(i))
-        ideal_functions = []
-        for i in indices:
-            ideal_functions.append(self.ideal.iloc[:, i])
-
-        ideal_functions = pandas.DataFrame(ideal_functions).transpose()
-        ideal_functions.columns = ['y1', 'y2', 'y3', 'y4']
-        return ideal_functions
-
+import all_classes as cl
 
 if __name__ == "__main__":
 
     """Creating instances of train, test and ideal classes"""
-    train = Train()
-    test = Test()
-    ideal = Ideal()
+    train = cl.Train()
+    test = cl.Test()
+    ideal = cl.Ideal()
 
     """Loading data"""
     train_data = train.load_training_data()
     test_data = test.load_test_data()
     ideal_data = ideal.load_ideal_data()
 
-    database = Database()
+    database = cl.Database()
 
     """Finding ideal functions"""
 
