41af94840de972dcd5f65a410e2edd31d3b3bfc3
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2023-04-08 20:00:16 +0530

    Code Modifications

--- sql_classes.py
+++ sql_classes.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float, create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
